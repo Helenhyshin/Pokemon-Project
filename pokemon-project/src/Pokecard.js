6a415606e0579aa627495760d9d76b9502117479
Helen Shin <helen@helens-air.lan>
2021-04-19 20:44:16 -0700

    initial commit

---
+++ pokemon-project/src/Pokecard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './Pokecard.css';
+
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+
+class Pokecard extends Component {
+    render(){
+        let pokeId = JSON.stringify(this.props.id);
+        let padToThree = pokeId.padStart(3, "0");
+        let imgSrc = `${POKE_API}${padToThree}.png`
+        return(
+            <div className="Pokecard">
+                <h1 className="Pokecard-title">{this.props.name}</h1>
+                <div className="Pokecard-image">
+                    <img src={imgSrc} alt={this.props.name}/>
+                </div>
+                <div className="Pokecard-data">Type: {this.props.type}</div>
+                <div className="Pokecard-data">EXP: {this.props.exp}</div>
+            </div>
+        );
+    }
+}
+
+export default Pokecard;
